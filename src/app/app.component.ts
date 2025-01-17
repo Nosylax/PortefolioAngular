29a7c368e8d05140394b5124a349644da47aa1b1
Nosylax <alyson.laville@gmail.com>
2025-01-17 15:06:51 +0100

    Initialisation projet + création component projets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProjetsComponent } from './projets/projets.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ProjetsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'PortefolioAngular';
+
 }
