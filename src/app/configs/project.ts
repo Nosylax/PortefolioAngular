3a0564ed7c69419ae220adebe9061905fb695cd8
Nosylax <alyson.laville@gmail.com>
2025-03-21 17:15:24 +0100

    ajout texte projet

--- src/app/configs/project.ts
+++ src/app/configs/project.ts
@@ -3,20 +3,20 @@ import { CardData } from "../models/card";
 
 export const getProjects = (): CardData[] => [
     {
-        title: 'Bookie',
-        description: 'Créez la page d\'accueil d\'une agence de voyage avec HTML & CSS',
+        title: 'Bookie : Page d\'accueil d\'une agence de voyage',
+        description: 'J\'ai créé la page d\'accueil d\'une agence de voyage en utilisant HTML et CSS, en intégrant une interface responsive basée sur des maquettes Figma pour mobile, tablette et desktop. J\'ai développé des composants d\'interface pour garantir une correspondance exacte avec le design et veiller à ce que le site fonctionne parfaitement sur toutes les tailles d\'écran. Ce projet m\'a permis de renforcer mes compétences en design responsive et en structuration de code pour le web.',
         imageUrl: "/booki.png",
         link: 'https://nosylax.github.io/BOOKI/'
     },
     {
-        title: 'Kasa',
-        description: 'Créez une application web de location immobilière avec React',
+        title: 'Kasa : Application de location de logement avec React',
+        description: 'J\'ai développé le front-end d\'une application de location de logement en utilisant React et React Router. J\'ai créé des composants pour afficher les informations des logements et configuré la navigation entre les pages de manière fluide. Le projet m\'a permis de renforcer mes compétences en gestion de l\'état avec les hooks et en développement d\'interfaces utilisateur modernes et réactives.',
         imageUrl: "/kasa.png",
         link: 'https://nosylax.github.io/kasa/'
     },
     {
-        title: 'Sophie Bluel',
-        description: 'Créez une page web dynamique avec JavaScript',
+        title: 'Sophie Bluel : Site web dynamique',
+        description: 'Dans ce projet, j\ai créé une page web dynamique pour le site d\'une architecte d\intérieur en utilisant JavaScript et en intégrant une API. J\ai développé la page de présentation des travaux de l\architecte, une page de connexion pour l\administrateur, et une modale pour uploader de nouveaux médias. Ce projet m\a permis de perfectionner mes compétences en manipulation du DOM, gestion des événements utilisateurs, et communication avec une API, tout en utilisant des outils comme Figma, Visual Studio Code, et GitHub.',
         imageUrl: "/SophieBluel.png",
         link: 'https://nosylax.github.io/sophie-bluel/'
     }
