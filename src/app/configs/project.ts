4db5d4f4a0ed748d5d1e9aa8ea41b41a7054e29b
Nosylax <alyson.laville@gmail.com>
2025-03-21 15:48:08 +0100

    modification de la banner et des titres

--- src/app/configs/project.ts
+++ src/app/configs/project.ts
@@ -4,19 +4,19 @@ import { CardData } from "../models/card";
 export const getProjects = (): CardData[] => [
     {
         title: 'Bookie',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at auctor libero. Curabitur erat est, sodales vitae erat vel, aliquam.',
+        description: 'Créez la page d\'accueil d\'une agence de voyage avec HTML & CSS',
         imageUrl: "/booki.png",
         link: 'https://nosylax.github.io/BOOKI/'
     },
     {
         title: 'Kasa',
-        description: 'Lorem amet dolor sit amet, consectetur adipiscing elit. Quisque at auctor libero. Curabitur erat est, sodales vitae erat vel, aliquam.',
+        description: 'Créez une application web de location immobilière avec React',
         imageUrl: "/kasa.png",
         link: 'https://nosylax.github.io/kasa/'
     },
     {
         title: 'Sophie Bluel',
-        description: 'Lorem amet dolor sit amet, consectetur adipiscing elit. Quisque at auctor libero. Curabitur erat est, sodales vitae erat vel, aliquam.',
+        description: 'Créez une page web dynamique avec JavaScript',
         imageUrl: "/SophieBluel.png",
         link: 'https://nosylax.github.io/sophie-bluel/'
     }
