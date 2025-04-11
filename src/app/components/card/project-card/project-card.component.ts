bfa575bd3043d6775ee5a35d6dae3dfb4ee4a976
Nosylax <alyson.laville@gmail.com>
2025-04-11 12:31:03 +0200

    correction bouton github

--- src/app/components/card/project-card/project-card.component.ts
+++ src/app/components/card/project-card/project-card.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { TitleCasePipe } from '@angular/common';
+import { NgIf, TitleCasePipe } from '@angular/common';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { CardData } from '../../../models/card';
 
 @Component({
   selector: 'app-project-card',
-  imports: [TitleCasePipe, MatProgressBarModule],
+  imports: [TitleCasePipe, MatProgressBarModule, NgIf],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
--- src/app/configs/project.ts
+++ src/app/configs/project.ts
@@ -15,7 +15,7 @@ export const getProjects = (): CardData[] => [
         description: 'J\'ai développé le front-end d\'une application de location de logement en utilisant React et React Router. J\'ai créé des composants pour afficher les informations des logements et configuré la navigation entre les pages de manière fluide. Le projet m\'a permis de renforcer mes compétences en gestion de l\'état avec les hooks et en développement d\'interfaces utilisateur modernes et réactives.',
         imageUrl: "/kasa.png",
         GHlink: 'https://github.com/Nosylax/kasa-app',
-        GHPlink: 'https://nosylax.github.io/kasa-app-master/'
+
 
 
     },
@@ -24,7 +24,6 @@ export const getProjects = (): CardData[] => [
         description: 'Dans ce projet, j\ai créé une page web dynamique pour le site d\'une architecte d\intérieur en utilisant JavaScript et en intégrant une API. J\ai développé la page de présentation des travaux de l\architecte, une page de connexion pour l\administrateur, et une modale pour uploader de nouveaux médias. Ce projet m\a permis de perfectionner mes compétences en manipulation du DOM, gestion des événements utilisateurs, et communication avec une API, tout en utilisant des outils comme Figma, Visual Studio Code, et GitHub.',
         imageUrl: "/SophieBluel.png",
         GHlink: 'https://github.com/Nosylax/Portfolio-architecte-sophie-bluel',
-        GHPlink: ''
 
     }
 ]
